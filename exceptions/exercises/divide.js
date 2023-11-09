787d7117b0ee84971cc61a33f47b9233232c2ba1
Steve Larson <stevelarson87@gmail.com>
2023-11-08 19:29:19 -0600

    modules done

--- exceptions/exercises/divide.js
+++ exceptions/exercises/divide.js
@@ -1,5 +1,5 @@
 // Write a function called 'divide' that takes two parameters: a numerator and a denominator.
-
+let divide = function(numerator, denominator){ return(numerator/denominator)}
 // Your function should return the result of numerator / denominator.
 
 // However, if the denominator is zero you should throw the error, "Attempted to divide by zero."
