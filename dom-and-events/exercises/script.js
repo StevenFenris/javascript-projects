8da0016bace1e54f1e73a64c621ca8e0b9233791
StevenFenris <stevelarson87@gmail.com>
2023-11-15 20:14:01 -0600

    completed exercise

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,12 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+   button.addEventListener("click", function() {
+        paragraph.innerHTML = "Houston, we have liftoff!";
+    });
+   missionAbort.addEventListener("mouseover", function() {
+        missionAbort.style.background = "red";
+    });    
 }
 
 window.addEventListener("load", init);
